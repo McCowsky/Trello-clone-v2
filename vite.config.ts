dc96b7465c80491645fbcf8b2fea961bdb2a35b0
McCowsky <makowskimateusz@outlook.com>
2023-02-02 20:41:44 +0100

    github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Trello-clone/",
+  base: "/Trello-clone-v2/",
   build: {
     chunkSizeWarningLimit: 1600,
     outDir: "build",
