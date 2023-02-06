ee1402d165f43123e270b2c0b128d2fce953cf92
McCowsky <makowskimateusz@outlook.com>
2023-02-06 03:09:21 +0100

    added index files for components

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,7 @@
 import { Route } from "@tanstack/react-location";
-import Home from "@/views/home/Home";
-import BoardWrapper from "@/views/board/BoardWrapper";
+//import BoardWrapper from "@/views/board/BoardWrapper";
+import { BoardWrapper } from "@/views/board";
+import { Home } from "@/views/home";
 export const routes: Route[] = [
   {
     path: "/Trello-clone-v2",
